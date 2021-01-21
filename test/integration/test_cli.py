0857cd5cbbd861d008c3e26b4f131d9dfe5bcc31
rMarinf <rmarinfdez@gmail.com>
2021-01-21 12:35:52 +0100

    Improve CLI integration tests

--- test/integration/test_cli.py
+++ test/integration/test_cli.py
@@ -9,7 +9,18 @@ class TestCLI:
             html_response = html_response_file.read()
             runner = CliRunner()
             requests_mock.get('https://www.crossfit.com/210121', text=html_response)
+            result = runner.invoke(cli, ['--day', '2021/01/21'])
+            assert result.exit_code == 0
+            assert '210121' in result.output
+            assert 'Rest Day' in result.output
+
             result = runner.invoke(cli)
             assert result.exit_code == 0
             assert '210121' in result.output
             assert 'Rest Day' in result.output
+
+    def test_cli_bad_day(self):
+        runner = CliRunner()
+        result = runner.invoke(cli, ['--day', '2021-02-22'])
+        assert result.exit_code == 0
+        assert 'Invalid date format' in result.output
