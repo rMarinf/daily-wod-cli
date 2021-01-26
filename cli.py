8cf97ae89df143ee8c7c947744dff4efa9be72cb
rMarinf <rmarinfdez@gmail.com>
2021-01-26 18:18:36 +0100

    Add show-content option

--- cli.py
+++ cli.py
@@ -7,7 +7,8 @@ cli_controller = CLIController()
 
 @click.command()
 @click.option('--day', '-d', help='Select the day of WOD (format:yyyy/mm/dd)')
-def cli(day):
+@click.option('--show-content', '-sc', help='Show info in tty or telegram channel (default: tty)', default='tty')
+def cli(day, show_content):
     """Get the WOD from Crossfit Web"""
-    result = cli_controller.get(day)
+    result = cli_controller.get(day, show_content)
     click.echo(result)
--- test/integration/test_cli.py
+++ test/integration/test_cli.py
@@ -1,3 +1,6 @@
+import json
+import re
+
 from click.testing import CliRunner
 from datetime import date
 from cli import cli
@@ -5,7 +8,7 @@ from cli import cli
 
 class TestCLI:
 
-    def test_cli(self, requests_mock):
+    def test_cli_tty(self, requests_mock):
         with open('test/integration/mocks/response.html', 'r') as html_response_file:
             html_response = html_response_file.read()
             runner = CliRunner()
@@ -21,6 +24,20 @@ class TestCLI:
             assert result.exit_code == 0
             assert today_str in result.output
             assert 'Rest Day' in result.output
+
+    def test_cli_telegram(self, requests_mock):
+        with open('test/integration/mocks/response.html', 'r') as html_response_file:
+            with open('test/integration/mocks/telegram-response.json', 'r') as telegram_file:
+                html_response = html_response_file.read()
+                telegram_json_data = json.dumps(telegram_file.read())
+                runner = CliRunner()
+                requests_mock.get('https://www.crossfit.com/210121', text=html_response)
+                matcher = re.compile('https://api.telegram.org/bot*')
+                requests_mock.get(matcher, json=telegram_json_data)
+                result = runner.invoke(cli, ['--day', '2021/01/21', '--show-content', 'telegram'])
+                print(result)
+                assert result.exit_code == 0
+                assert 'Go to @DailyWOD channel to see the WOD =)\n' in result.output
 
     def test_cli_bad_day(self):
         runner = CliRunner()
